e9a9c53261623548c9f56587e57dbc885e829cf3
mahrad <mahradti@gmail.com>
2024-04-13 16:52:35 +0330

    Spinner W/Conditional Rendering

--- src/components/CoinPage.jsx
+++ src/components/CoinPage.jsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios"
-import {useParams} from "react-router-dom"
+import {useNavigate, useParams} from "react-router-dom"
+import { IoArrowBackCircleOutline } from "react-icons/io5";
 
 const CoinPage = () => {
 
     const [coin , setCoin] = useState({})
+
+    const navigate = useNavigate()
     
     const params = useParams()
     
     const URL = `https://api.coingecko.com/api/v3/coins/${params.coinId}`
+    
+    const handleNavigate = ()=>{
+        navigate("/")
+    }
 
     useEffect(()=>{
         axios.get(URL)
@@ -20,9 +27,12 @@ const CoinPage = () => {
             console.log(err);
         })
     } , [])
-
+    
     return (
         <div>
+            <div className='flex sm:justify-center lg:justify-start mt-5'>
+                <p className='bg-slate-700 text-white font-bold p-2 rounded-lg cursor-pointer flex items-center gap-x-3' onClick={handleNavigate}>BACK <IoArrowBackCircleOutline className='text-2xl'/></p>
+            </div>
             {coin.market_data ? (
                 <div className='max-w-[868px] mx-auto mt-16'>
                     <div className='bg-slate-800 py-6 shadow-lg rounded-lg'>
@@ -138,7 +148,18 @@ const CoinPage = () => {
                         </div>
                     </div>
                 </div>
-            ) : null}
+            ) : (
+                
+
+                <button disabled type="button" className="mt-12 py-2.5 px-5 me-2 text-xl font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center">
+                <svg aria-hidden="true" role="status" className="inline w-6 h-6 me-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2"/>
+                </svg>
+                Loading...
+                </button>
+
+            )}
         </div>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Logo from "../assets/coincheck-logo.png"
+import Logo from "../assets/logo2.png"
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <Link to='/'>
-            <div className='flex justify-center items-center w-full'>
-                <img src={Logo} className='w-[200px] h-32 rounded-full border-blue-950 border-2' alt="" />
+            <div className='flex justify-center items-center w-full bg-none'>
+                <img src={Logo} className='w-[200px] h-32 rounded-full border-gray-700 border-2' alt="" />
             </div>
         </Link>
     );
